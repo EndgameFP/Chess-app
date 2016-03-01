c91ceb98d1e8c4dd541dfb8163b875a300725deb
Shawydu <dxydyl2@gmail.com>
2016-02-29 20:39:03 -0500

    add board page

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228183438) do
+ActiveRecord::Schema.define(version: 20160229152809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "name"
+    t.string   "status"
+    t.integer  "white_player_id"
+    t.integer  "black_player_id"
+    t.integer  "turn_player_id"
   end
 
   create_table "users", force: :cascade do |t|
