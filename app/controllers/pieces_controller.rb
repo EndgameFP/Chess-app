850123b4cbc22c565c3e0b4f1e86056e904c3be8
Mike Stone <michaeljacobstone@gmail.com>
2016-03-18 04:33:56 +0000

    clean up

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -5,17 +5,11 @@ class PiecesController < ApplicationController
 		@current = Piece.find_by_id(params[:id])
 		@game = @current.game
 		@pieces = @game.pieces
-		
-		logger.debug "HII"
 		@pieces.each do |piece|
-			logger.debug piece.x_position==params[:x_position]
-			logger.debug piece.y_position==params[:y_position]
 			if piece.x_position.to_i==params[:x_position].to_i&&piece.y_position.to_i==params[:y_position].to_i
-			    logger.info("GO OVER")
 				return respond_to do |format|
 					format.json { render :json => {:message => "Fail",:status => :not_found } }
 				end
-				
 			end
 		end
 
