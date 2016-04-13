71c46b15a586ba2c7a273690c8a2db37ed9dcec6
unyon <michalloran@hotmail.com>
2016-04-13 20:00:52 +0000

    codeship

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160407174522) do
-=======
-
-ActiveRecord::Schema.define(version: 20160405200957) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
