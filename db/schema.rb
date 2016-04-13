6dcbf7cc953a499116945a3b2042f9e0b7199f8f
unyon <michalloran@hotmail.com>
2016-04-13 19:50:23 +0000

    merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404124710) do
+ActiveRecord::Schema.define(version: 20160407174522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.string   "name"
     t.string   "status"
     t.integer  "white_player_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160404124710) do
     t.integer  "turn_player_id"
     t.integer  "game_id"
     t.integer  "turn"
+    t.integer  "last_move",       default: [],              array: true
   end
 
   create_table "pieces", force: :cascade do |t|
@@ -60,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160404124710) do
     t.integer  "losses"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
